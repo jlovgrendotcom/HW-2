c1ba869a41c411e5e80eef2dd8eaad97040526fd
jlovgrendotcom <jmlovgren@gmail.com>
2021-04-20 22:00:17 -0700

    updating HW-2 with bug fixes and reformatting with anonymous functions

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 
          
-let listArray = [];
-document.getElementById('changeWordsBtn').style.visibility='hidden';
+let firstArray = [];
 
-function makeList() {
-    let ul = document.createElement('ul');
-    let size = 3;
+document.addEventListener("DOMContentLoaded", function (event) {
 
-        for(let i = 0; i<size; i++) {
-            listArray[i] = prompt('Please enter a word ');
-            document.getElementById('myList').appendChild(ul);
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.innerHTML = listArray[i];  
+    document.getElementById("ChangedWords").style.display = "none";
+    document.getElementById("changeWords").style.visibility = "hidden";
+
+    document.getElementById("start").addEventListener("click", StartProgram, false);
+    document.getElementById("changeWords").addEventListener("click", ChangeWords, false);
+});
+
+function StartProgram() {
+
+    document.getElementById("start").style.visibility = "hidden";
+    let firstUl = document.getElementById("list1");
+
+        for(let i = 0; i < 3; i++) {
+            firstArray.push(prompt("Please enter a word"));
+            let anotherLi = document.createElement("li"); 
+            anotherLi.appendChild(document.createTextNode(firstArray[i]));
+            firstUl.appendChild(anotherLi);
     } 
-    displayChangeBtn(); 
+    document.getElementById("changeWords").style.visibility = "visible";
 }
 
-function displayChangeBtn() {
-    document.getElementById('changeWordsBtn').style.visibility = 'visible';
+function ChangeWords() {
+
+    document.getElementById("GetWords").style.display = "none";
+    document.getElementById("ChangedWords").style.display = "block";
+   
+    let secondArray = firstArray.map(function(oneWord) {
+        return oneWord.charAt(oneWord.length - 1) 
+        + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); }); 
+
+    let secondUl = document.getElementById("list2");  
+
+    secondArray.forEach(function(element) {
+        let secondLi = document.createElement("li");
+        secondLi.appendChild(document.createTextNode(element));
+        secondUl.appendChild(secondLi);
+    }); 
 }
-
-document.getElementById('transformedListScreen').style.display = 'none';
-changeWordsBtn.addEventListener("click",changeWords,false);
-
-function changeWords() {
-    document.getElementById('changeWordsBtn').style.visibility = 'hidden';
-    let ul = document.createElement('ul');  
-
-    let newArray = listArray.map(function(newList) {
-        return newList.charAt(newList.length - 1)
-        + newList.substring(1, newList.length - 1) + newList.charAt(0); }); 
-
-        for(let j = 0; j < newArray.length; j++) {
-            document.getElementById('myList').appendChild(ul);
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.innerHTML = newArray[j]; 
-    }
-}
-
 
